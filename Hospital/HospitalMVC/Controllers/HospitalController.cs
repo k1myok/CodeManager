bcc146d70ba20cfcb07b5e5d416682c7dfb5a0df
menghuantianya <yangjuntao90@163.com>
2016-03-04 18:10:33 +0800

    增加controller里面的方法

--- Hospital/HospitalMVC/Controllers/HospitalController.cs
+++ Hospital/HospitalMVC/Controllers/HospitalController.cs
@@ -16,12 +16,67 @@ namespace HospitalMVC.Controllers
         {
             return View();
         }
-        public string[] GetHospitalList()
+        public ActionResult  GetHospitalList()
         {
+            //Hospital hos= new Hospital();
+            ViewBag.List = new List<Hospital>();
             var context = new CityServiceEntities();
-            var result = context.Hospitals.OrderBy(p => p.HospName).Select(p => p.HospName).ToArray();
-            return result;
+            var result = context.Hospitals.OrderBy(p => p.HospName).Select(p => p.HospName).ToList();
+            var list = (List<Hospital>)ViewBag.list;
+            return ViewBag(list);
         }
+        /// <summary>
+        ///专家科室挂号
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ExRegister()
+        {
 
+            return View();
+        }
+        /// <summary>
+        /// 普通科室挂号
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ComRegister()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 获取专家科室列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetExDepart()
+        {
+
+
+            return View();
+        }
+        /// <summary>
+        /// 获取普通科室列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetComDepart()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 获取专家科室号源
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetExRegister()
+        {
+
+
+            return View();
+        }
+        /// <summary>
+        /// 获取普通科室号源
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetExRegister()
+        {
+            return View();
+        }
     }
 }
