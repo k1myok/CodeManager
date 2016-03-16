19cca828d013ee561356332f4b918ef86ab67059
menghuantianya <yangjuntao90@163.com>
2016-03-16 22:32:40 +0800

    add

--- Hospital/HospitalMVC/Global.asax.cs
+++ Hospital/HospitalMVC/Global.asax.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace HospitalMVC
 {
     // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -19,6 +21,8 @@ namespace HospitalMVC
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
--- Hospital/HospitalMVC/Properties/AssemblyInfo.cs
+++ Hospital/HospitalMVC/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("d214115c-bd9b-46b8-b182-03c6edb3de29")]
+[assembly: Guid("3016f607-7daa-4fae-981c-0bb5178665e1")]
 
 // 程序集的版本信息由下列四个值组成:
 //
