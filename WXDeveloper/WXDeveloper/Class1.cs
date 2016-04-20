9fe0d1391bb62a99ddfb1c48c25d14d9e893f641
menghuantianya <yangjuntao90@163.com>
2016-04-20 17:30:35 +0800

    add the wechat code

---
+++ WXDeveloper/WXDeveloper/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WXDeveloper
+{
+    public class WX
+    {
+        public string Access_token { set; get; }
+        public string express_in { set; get; }
+    }
+}
