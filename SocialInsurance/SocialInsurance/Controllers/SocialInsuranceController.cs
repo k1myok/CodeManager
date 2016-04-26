e643776fdd4d449c7304969362e91a211edb94cf
menghuantianya <yangjuntao90@163.com>
2016-04-26 14:22:00 +0800

    add

--- SocialInsurance/SocialInsurance/Controllers/SocialInsuranceController.cs
+++ SocialInsurance/SocialInsurance/Controllers/SocialInsuranceController.cs
@@ -11,18 +11,24 @@ namespace SocialInsurance.Controllers
 {
     public class SocialInsuranceController : Controller
     {
+    
+
         //
         public ActionResult Default()
         {
             return PartialView();
         }
+       /// <summary>
+       /// 企业养老缴费信息查询
+       /// </summary>
+       /// <param name="con"></param>
+       /// <returns></returns>
         [HttpPost]
         public ActionResult PersonalInsurance(condition con)
         {
-            var client = new SocialInsuranceService.ShbServClient();
+           var client = new SocialInsuranceService.ShbServClient();
             var temp = client.si010201(con.ID.Trim(), con.IDCard.Trim(), 8, 1);
             XElement xoc = XElement.Parse(temp);
-           // List <PersonalInsurance> data = new List<PersonalInsurance>();
             PersonalInsurance p = new PersonalInsurance();
             List<InsuranceDetail> data = new List<InsuranceDetail>();
             var faultcode = xoc.Descendants("faultcode").ToList();
@@ -39,7 +45,6 @@ namespace SocialInsurance.Controllers
                 p.pages = resu[0].Attribute("pages").Value.ToString();
                 p.cpage = resu[1].Attribute("cpage").Value.ToString();
                 p.rowcount = resu[2].Attribute("rowcount").Value.ToString();
-               
                 foreach (var item in re)
                 {
                     InsuranceDetail Insurance = new InsuranceDetail();
@@ -50,28 +55,24 @@ namespace SocialInsurance.Controllers
                     Insurance.aif004 = item.Attribute("aif004").Value;
                     Insurance.aif005 = item.Attribute("aif005").Value;
                     data.Add(Insurance);
-                }
-                
-               
-               
-                //foreach (var item in re)
-                //{
-                //    p.pages = resu[0].Attribute("pages").Value.ToString();
-                //    p.cpage = resu[1].Attribute("cpage").Value.ToString();
-                //    p.rowcount = resu[2].Attribute("rowcount").Value.ToString();
-                //    p.status = "100";
-                //    p.aab004 = item.Attribute("aif005").Value;
-                //    p.aac001 = item.Attribute("aac001").Value;
-                //    p.aif005 = item.Attribute("aif005").Value;
-                //    p.aif004 = item.Attribute("aif004").Value;
-                //    p.aae001 = item.Attribute("aae001").Value;
-                //    p.aac003 = item.Attribute("aac003").Value;
-                //    data.Add(p);
-                //}
-                
+                } 
             }
             p.msg = data;
             return View(p);
+        }
+        /// <summary>
+        /// 个人参保情况查询
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="IDCard"></param>
+        [HttpPost]
+        public  PartialViewResult PersonInsuranceStatus(string ID,string IDCard)
+        {
+            ID = "0500708293";
+            IDCard ="441381198208204752";
+            var client = new SocialInsuranceService.ShbServClient();
+            var result = client.si120101(ID,IDCard,8,1);
+            return PartialView(result);
         }
     }
 }
