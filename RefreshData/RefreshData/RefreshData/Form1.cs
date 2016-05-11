bd00ea531b71d7997cd45613ba629f29bd0cfc92
yangjuntao90@163.com <yang1149797641>
2016-05-11 22:49:38 +0800

    add the code for get the webdata

---
+++ RefreshData/RefreshData/RefreshData/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RefreshData;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Xml;
+using System.Xml.Linq;
+namespace RefreshData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = HttpHelper.GetHtmlExByByPost("http://rss.weibodangan.com/weibo/rss/1822107334/", "",Encoding.UTF8);
+            if (result != null)
+            {
+                var sql="data source=.;initial catalog=RefreshData;user id=yang;pwd=kenyang123!@#";
+                var connection = new SqlConnection(sql);
+                var adapter = new SqlDataAdapter("select top 1 * from SinaData where 0>1", connection);
+                var table = new DataTable();
+                adapter.Fill(table);
+                XElement temp = XElement.Parse(result);
+                var itemrow = temp.Descendants("item").ToList();
+                foreach (var list in itemrow)
+                {
+                    var row = table.NewRow();
+                    row["title"] = list.Element("title").Value;
+                    row["description"] = list.Element("description").Value;
+                    row["pubDate"] = list.Element("pubDate").Value;
+                    row["guid"]= list.Element("guid").Value;
+                    row ["link"] = list.Element("link").Value;
+                    table.Rows.Add(row);
+                }
+
+                var command = new SqlCommand("delete from SinaData where 0>1", connection);
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                if (command.ExecuteNonQuery() > -1)
+                {
+                    var builder = new SqlCommandBuilder(adapter);
+                    adapter.InsertCommand = builder.GetInsertCommand();
+                    adapter.Update(table);
+                 
+                }
+
+
+
+            }
+
+        }
+    }
+}
