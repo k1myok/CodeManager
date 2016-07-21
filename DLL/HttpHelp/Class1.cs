7fa83610547f587ca21b0b524fc65cd39927a8cd
fuhuayishiyanyun <fuhuayishiyanyun@gmail.com>
2016-07-21 18:47:30 +0800

    add httphelp and file info

---
+++ DLL/HttpHelp/Class1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace HttpHelp
+{
+    public class HttpHelper
+    {
+        public static string GetHtmlEx(string url, Encoding encoding)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "get";
+            request.UserAgent = @"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.04506.30)";
+            WebResponse response = request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream, encoding);
+            String html = reader.ReadToEnd();
+            response.Close();
+
+            return html;
+        }
+        public static string GetHtmlExByByPost(string postUrl, string paramData, Encoding dataEncode)
+        {
+            string ret = string.Empty;
+            try
+            {
+                byte[] byteArray = dataEncode.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                //webReq.ContentType = "application/x-www-form-urlencoded";
+
+                webReq.UserAgent = @"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.04506.30)";
+
+                webReq.ContentLength = byteArray.Length;
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), dataEncode);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return ret;
+        }
+        public static string GetHtmlExByByPost(string postUrl, string paramData, Encoding dataEncode, Action<HttpWebRequest> action)
+        {
+            string ret = string.Empty;
+            try
+            {
+                byte[] byteArray = dataEncode.GetBytes(paramData); //转化
+                HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
+                webReq.Method = "POST";
+                webReq.ContentType = "application/x-www-form-urlencoded";
+                webReq.UserAgent = @"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.0.04506.30)";
+                //webReq.Accept ="image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*";
+                webReq.ContentLength = byteArray.Length;
+                if (action != null)
+                    action(webReq);
+                Stream newStream = webReq.GetRequestStream();
+                newStream.Write(byteArray, 0, byteArray.Length);//写入参数
+                newStream.Close();
+                HttpWebResponse response = (HttpWebResponse)webReq.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream(), dataEncode);
+                ret = sr.ReadToEnd();
+                sr.Close();
+                response.Close();
+                newStream.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return ret;
+        }
+        public static System.DateTime ConvertIntDateTime(double milliseconds)
+        {
+            System.DateTime time = System.DateTime.MinValue;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            time = startTime.AddMilliseconds(milliseconds);
+            return time;
+        }
+    }
+    public class DealWithFile
+    {
+
+
+        /// <summary>
+        /// 删除文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void DeleteFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        public static void WtiteLog(string path,string mes)
+        {
+           
+            var filepath = path + @"123.txt";
+
+            if (!File.Exists(filepath))
+            {
+                File.CreateText(filepath);
+                
+                File.AppendAllLines(filepath,new string[] {mes},Encoding.UTF8);
+                  //  (filepath,mes,);
+                //FileStream fs1 = new FileStream(filepath, FileMode.Create, FileAccess.Write);
+                //StreamWriter sw = new StreamWriter(fs1);
+                //sw.WriteLine(mes);
+                //sw.Close();
+                //fs1.Close();
+                
+            }
+            else
+            {
+                File.AppendAllLines(filepath, new string[] { mes }, Encoding.UTF8);
+                //FileStream fs = new FileStream(filepath, FileMode.Append, FileAccess.Write);
+                //StreamWriter sr = new StreamWriter(fs);
+                //sr.WriteLine(mes);//开始写入值
+                //sr.Close();
+                //fs.Close();
+            }
+
+        }
+        /// <summary>
+        /// 复制文件夹中的所有文件夹与文件到另一个文件夹
+        /// </summary>
+        /// <param name="sourcePath">源文件夹</param>
+        /// <param name="destPath">目标文件夹</param>
+        public static void CopyFolder(string sourcePath, string destPath)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败：" + ex.Message);
+                    }
+                }
+                //获得源文件下所有文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    File.Copy(c, destFile, true);//覆盖模式
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //采用递归的方法实现
+                    CopyFolder(c, destDir);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在！");
+            }
+        }
+
+        /// <summary>
+        /// 移动文件夹中的所有文件夹与文件到另一个文件夹
+        /// </summary>
+        /// <param name="sourcePath">源文件夹</param>
+        /// <param name="destPath">目标文件夹</param>
+        public static void MoveFolder(string sourcePath, string destPath)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败：" + ex.Message);
+                    }
+                }
+                //获得源文件下所有文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //覆盖模式
+                    if (File.Exists(destFile))
+                    {
+                        File.Delete(destFile);
+                    }
+                    File.Move(c, destFile);
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //Directory.Move必须要在同一个根目录下移动才有效，不能在不同卷中移动。
+                    //Directory.Move(c, destDir);
+
+                    //采用递归的方法实现
+                    MoveFolder(c, destDir);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在！");
+            }
+        }
+
+       
+        /// <summary>
+        /// 删除指定目录下所有内容：方法一--删除目录，再创建空目录
+        /// </summary>
+        /// <param name="dirPath"></param>
+        public static void DeleteFolderEx(string dirPath)
+        {
+            if (Directory.Exists(dirPath))
+            {
+                Directory.Delete(dirPath);
+                Directory.CreateDirectory(dirPath);
+            }
+        }
+
+        /// <summary>
+        /// 删除指定目录下所有内容：方法二--找到所有文件和子文件夹删除
+        /// </summary>
+        /// <param name="dirPath"></param>
+        public static void DeleteFolder(string dirPath)
+        {
+            if (Directory.Exists(dirPath))
+            {
+                foreach (string content in Directory.GetFileSystemEntries(dirPath))
+                {
+                    if (Directory.Exists(content))
+                    {
+                        Directory.Delete(content, true);
+                    }
+                    else if (File.Exists(content))
+                    {
+                        File.Delete(content);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
