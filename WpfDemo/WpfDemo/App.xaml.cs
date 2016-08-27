a95f1d82c66b4ac0ee92959c95144e3123cc61be
yangjuntao90@163.com <yang1149797641>
2016-08-27 22:50:19 +0800

    adds

---
+++ WpfDemo/WpfDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfDemo
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+         {
+             base.OnStartup(e);
+
+            //注册Application_Error
+            this.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+        } 
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //处理完后，我们需要将Handler=true表示已此异常已处理过
+             e.Handled = true;
+        }
+    }
+}
---
+++ YunLianLHelp/YunLianLHelp/EncryptionHelp.cs
@@ -0,0 +1,461 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.IO; 
+
+namespace YunLianLHelp
+{
+    public class EncryptionHelp
+    {
+
+       
+        #region Base64加密解密
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <returns></returns>
+        public static string Base64Encrypt(string input)
+        {
+            return Base64Encrypt(input, new UTF8Encoding());
+        }
+
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <param name="encode">字符编码</param>
+        /// <returns></returns>
+        public static string Base64Encrypt(string input, Encoding encode)
+        {
+            return Convert.ToBase64String(encode.GetBytes(input));
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="input">需要解密的字符串</param>
+        /// <returns></returns>
+        public static string Base64Decrypt(string input)
+        {
+            return Base64Decrypt(input, new UTF8Encoding());
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="input">需要解密的字符串</param>
+        /// <param name="encode">字符的编码</param>
+        /// <returns></returns>
+        public static string Base64Decrypt(string input, Encoding encode)
+        {
+            return encode.GetString(Convert.FromBase64String(input));
+        }
+        #endregion
+
+        #region DES加密解密
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">加密数据</param>
+        /// <param name="key">8位字符的密钥字符串</param>
+        /// <param name="iv">8位字符的初始化向量字符串</param>
+        /// <returns></returns>
+        public static string DESEncrypt(string data, string key, string iv)
+        {
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">解密数据</param>
+        /// <param name="key">8位字符的密钥字符串(需要和加密时相同)</param>
+        /// <param name="iv">8位字符的初始化向量字符串(需要和加密时相同)</param>
+        /// <returns></returns>
+        public static string DESDecrypt(string data, string key, string iv)
+        {
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(iv);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return null;
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream(byEnc);
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIV), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cst);
+            return sr.ReadToEnd();
+        }
+        #endregion
+
+        #region MD5加密
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <returns></returns>
+        public static string MD5Encrypt(string input)
+        {
+            return MD5Encrypt(input, new UTF8Encoding());
+        }
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="input">需要加密的字符串</param>
+        /// <param name="encode">字符的编码</param>
+        /// <returns></returns>
+        public static string MD5Encrypt(string input, Encoding encode)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] t = md5.ComputeHash(encode.GetBytes(input));
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < t.Length; i++)
+                sb.Append(t[i].ToString("x").PadLeft(2, '0'));
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// MD5对文件流加密
+        /// </summary>
+        /// <param name="sr"></param>
+        /// <returns></returns>
+        public static string MD5Encrypt(Stream stream)
+        {
+            MD5 md5serv = MD5CryptoServiceProvider.Create();
+            byte[] buffer = md5serv.ComputeHash(stream);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte var in buffer)
+                sb.Append(var.ToString("x2"));
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// MD5加密(返回16位加密串)
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="encode"></param>
+        /// <returns></returns>
+        public static string MD5Encrypt16(string input, Encoding encode)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string result = BitConverter.ToString(md5.ComputeHash(encode.GetBytes(input)), 4, 8);
+            result = result.Replace("-", "");
+            return result;
+        }
+        #endregion
+
+        #region 3DES 加密解密
+
+        public static string DES3Encrypt(string data, string key)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            DES.Mode = CipherMode.CBC;
+            DES.Padding = PaddingMode.PKCS7;
+
+            ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+
+            byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(data);
+            return Convert.ToBase64String(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+
+        public static string DES3Decrypt(string data, string key)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            DES.Mode = CipherMode.CBC;
+            DES.Padding = System.Security.Cryptography.PaddingMode.PKCS7;
+
+            ICryptoTransform DESDecrypt = DES.CreateDecryptor();
+
+            string result = "";
+            try
+            {
+                byte[] Buffer = Convert.FromBase64String(data);
+                result = ASCIIEncoding.ASCII.GetString(DESDecrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+            }
+            catch (Exception e)
+            {
+
+            }
+            return result;
+        }
+
+        #endregion
+        public string Encode(string data)
+        {
+            var KEY_64 = "ASDWESX";
+            var IV_64 = "AQWESDXC";
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(KEY_64);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV_64);
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+
+        }
+        public string Decode(string data)
+        {
+            var KEY_64 = "QQQQQQQQ";
+            var IV_64 = "AAAAAAAA";
+            byte[] byKey = System.Text.ASCIIEncoding.ASCII.GetBytes(KEY_64);
+            byte[] byIV = System.Text.ASCIIEncoding.ASCII.GetBytes(IV_64);
+
+            byte[] byEnc;
+            try
+            {
+                byEnc = Convert.FromBase64String(data);
+            }
+            catch
+            {
+                return null;
+            }
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            MemoryStream ms = new MemoryStream(byEnc);
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateDecryptor(byKey, byIV), CryptoStreamMode.Read);
+            StreamReader sr = new StreamReader(cst);
+            return sr.ReadToEnd();
+        }
+        public string GetMD5_32(string s, string _input_charset)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] t = md5.ComputeHash(Encoding.GetEncoding(_input_charset).GetBytes(s));
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < t.Length; i++)
+            {
+                sb.Append(t[i].ToString("x").PadLeft(2, '0'));
+            }
+            return sb.ToString();
+        }
+
+        //16位加密 
+        public static string GetMd5_16(string ConvertString)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string t2 = BitConverter.ToString(md5.ComputeHash(UTF8Encoding.Default.GetBytes(ConvertString)), 4, 8);
+            t2 = t2.Replace("-", "");
+            return t2;
+        }
+        private static byte[] Keys = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+        /// <summary> 
+        /// DES加密字符串 
+        /// </summary> 
+        /// <param name="encryptString">待加密的字符串</param> 
+        /// <param name="encryptKey">加密密钥,要求为8位</param> 
+        /// <returns>加密成功返回加密后的字符串，失败返回源串</returns> 
+        public static string EncryptDES(string encryptString, string encryptKey)
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(encryptString);
+                DESCryptoServiceProvider dCSP = new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, dCSP.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Convert.ToBase64String(mStream.ToArray());
+            }
+            catch
+            {
+                return encryptString;
+            }
+        }
+        /// <summary> 
+        /// DES解密字符串 
+        /// </summary> 
+        /// <param name="decryptString">待解密的字符串</param> 
+        /// <param name="decryptKey">解密密钥,要求为8位,和加密密钥相同</param> 
+        /// <returns>解密成功返回解密后的字符串，失败返源串</returns> 
+        public static string DecryptDES(string decryptString, string decryptKey)
+        {
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(decryptKey);
+                byte[] rgbIV = Keys;
+                byte[] inputByteArray = Convert.FromBase64String(decryptString);
+                DESCryptoServiceProvider DCSP = new DESCryptoServiceProvider();
+                MemoryStream mStream = new MemoryStream();
+                CryptoStream cStream = new CryptoStream(mStream, DCSP.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                cStream.FlushFinalBlock();
+                return Encoding.UTF8.GetString(mStream.ToArray());
+            }
+            catch
+            {
+                return decryptString;
+            }
+        }
+        //加密文件 
+        private static void EncryptData(String inName, String outName, byte[] desKey, byte[] desIV)
+        {
+            //Create the file streams to handle the input and output files. 
+            FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+
+            //Create variables to help with read and write. 
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption. 
+            long rdlen = 0;              //This is the total number of bytes written. 
+            long totlen = fin.Length;    //This is the total length of the input file. 
+            int len;                     //This is the number of bytes to be written at a time. 
+
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(fout, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+
+            //Read from the input file, then encrypt and write to the output file. 
+            while (rdlen < totlen)
+            {
+                len = fin.Read(bin, 0, 100);
+                encStream.Write(bin, 0, len);
+                rdlen = rdlen + len;
+            }
+
+            encStream.Close();
+            fout.Close();
+            fin.Close();
+        }
+
+        //解密文件 
+        private static void DecryptData(String inName, String outName, byte[] desKey, byte[] desIV)
+        {
+            //Create the file streams to handle the input and output files. 
+            FileStream fin = new FileStream(inName, FileMode.Open, FileAccess.Read);
+            FileStream fout = new FileStream(outName, FileMode.OpenOrCreate, FileAccess.Write);
+            fout.SetLength(0);
+
+            //Create variables to help with read and write. 
+            byte[] bin = new byte[100]; //This is intermediate storage for the encryption. 
+            long rdlen = 0;              //This is the total number of bytes written. 
+            long totlen = fin.Length;    //This is the total length of the input file. 
+            int len;                     //This is the number of bytes to be written at a time. 
+
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encStream = new CryptoStream(fout, des.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
+
+            //Read from the input file, then encrypt and write to the output file. 
+            while (rdlen < totlen)
+            {
+                len = fin.Read(bin, 0, 100);
+                encStream.Write(bin, 0, len);
+                rdlen = rdlen + len;
+            }
+
+            encStream.Close();
+            fout.Close();
+            fin.Close();
+
+        }
+        /// <summary>
+        /// DES加密方法
+        /// </summary>
+        /// <param name="strPlain">明文</param>
+        /// <param name="strDESKey">密钥</param>
+        /// <param name="strDESIV">向量</param>
+        /// <returns>密文</returns>
+        public string DESFileEncrypt(string strPlain, string strDESKey, string strDESIV)
+        {
+            //把密钥转换成字节数组
+            byte[] bytesDESKey = ASCIIEncoding.ASCII.GetBytes(strDESKey);
+            //把向量转换成字节数组
+            byte[] bytesDESIV = ASCIIEncoding.ASCII.GetBytes(strDESIV);
+            //声明1个新的DES对象
+            DESCryptoServiceProvider desEncrypt = new DESCryptoServiceProvider();
+            //开辟一块内存流
+            MemoryStream msEncrypt = new MemoryStream();
+            //把内存流对象包装成加密流对象
+            CryptoStream csEncrypt = new CryptoStream(msEncrypt, desEncrypt.CreateEncryptor(bytesDESKey, bytesDESIV), CryptoStreamMode.Write);
+            //把加密流对象包装成写入流对象
+            StreamWriter swEncrypt = new StreamWriter(csEncrypt);
+            //写入流对象写入明文
+            swEncrypt.WriteLine(strPlain);
+            //写入流关闭
+            swEncrypt.Close();
+            //加密流关闭
+            csEncrypt.Close();
+            //把内存流转换成字节数组，内存流现在已经是密文了
+            byte[] bytesCipher = msEncrypt.ToArray();
+            //内存流关闭
+            msEncrypt.Close();
+            //把密文字节数组转换为字符串，并返回
+            return UnicodeEncoding.Unicode.GetString(bytesCipher);
+        }
+
+
+
+
+        /// <summary>
+        /// DES解密方法
+        /// </summary>
+        /// <param name="strCipher">密文</param>
+        /// <param name="strDESKey">密钥</param>
+        /// <param name="strDESIV">向量</param>
+        /// <returns>明文</returns>
+        public string DESTDecrypt(string strCipher, string strDESKey, string strDESIV)
+        {
+            //把密钥转换成字节数组
+            byte[] bytesDESKey = ASCIIEncoding.ASCII.GetBytes(strDESKey);
+            //把向量转换成字节数组
+            byte[] bytesDESIV = ASCIIEncoding.ASCII.GetBytes(strDESIV);
+            //把密文转换成字节数组
+            byte[] bytesCipher = UnicodeEncoding.Unicode.GetBytes(strCipher);
+            //声明1个新的DES对象
+            DESCryptoServiceProvider desDecrypt = new DESCryptoServiceProvider();
+            //开辟一块内存流，并存放密文字节数组
+            MemoryStream msDecrypt = new MemoryStream(bytesCipher);
+            //把内存流对象包装成解密流对象
+            CryptoStream csDecrypt = new CryptoStream(msDecrypt, desDecrypt.CreateDecryptor(bytesDESKey, bytesDESIV), CryptoStreamMode.Read);
+            //把解密流对象包装成读出流对象
+            StreamReader srDecrypt = new StreamReader(csDecrypt);
+            //明文=读出流的读出内容
+            string strPlainText = srDecrypt.ReadLine();
+            //读出流关闭
+            srDecrypt.Close();
+            //解密流关闭
+            csDecrypt.Close();
+            //内存流关闭
+            msDecrypt.Close();
+            //返回明文
+            return strPlainText;
+        }
+
+    }
+}
