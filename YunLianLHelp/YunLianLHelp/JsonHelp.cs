1a8bc1dc3e84bd4432ec897cc6a77eab72bdb365
yangjuntao90@163.com <yang1149797641>
2016-08-28 14:34:46 +0800

    添加httphelp

--- YunLianLHelp/YunLianLHelp/JsonHelp.cs
+++ YunLianLHelp/YunLianLHelp/JsonHelp.cs
@@ -5,11 +5,94 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using System.Web.Script.Serialization;
 namespace YunLianLHelp
 {
     public static class JsonHelper
     {
+        #region-----共用静态方法  Json.NET进行转换Json对象,推荐使用它，效率是JavaScriptSerializer的好几倍-----
+
+        /// <summary>
+        /// 将实体对象转换为Json对象，生成字符串
+        /// </summary>
+        /// <param name="item">实体对象</param>
+        /// <returns></returns>
+        public static string JsonNetSerializeToEntityInfo(object item)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(item);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 将Json串对象解析成固定的对象信息
+        /// </summary>
+        /// <typeparam name="T">需要得到解析后对象的实体类型</typeparam>
+        /// <param name="jsonStr">json串</param>
+        /// <returns></returns>
+        public static T JsonNetDeserializeToStringInfo<T>(string jsonStr)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region-----共用静态方法  JavaScript进行转换Json对象-----
+
+        /// <summary>
+        /// 将需要转换成json串的对象转换为Json字符串新消息
+        /// </summary>
+        /// <param name="item">需要实例化的对象</param>
+        /// <returns></returns>
+        public static string JavaScriptSerializeToEntityInfo(object item)
+        {
+            try
+            {
+                var javaScriptSerializer = new JavaScriptSerializer();
+                return javaScriptSerializer.Serialize(item);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 将指定的Json字符串转换为T类型的对象，JavaScript序列化
+        /// </summary>
+        /// <typeparam name="T">需要得到解析后的对象的实体类型</typeparam>
+        /// <param name="jsonStr">进行反序列化的Json字符串</param>
+        /// <returns></returns>
+        public static T JavaScriptSerializeToStringInfo<T>(string jsonStr)
+        {
+            try
+            {
+                var javaScriptSerializer = new JavaScriptSerializer();
+                return javaScriptSerializer.Deserialize<T>(jsonStr);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
+
+        
+
         public static string ToJsonFrYL(DataTable dt)
         {
             StringBuilder jsonString = new StringBuilder();
@@ -198,5 +281,6 @@ namespace YunLianLHelp
             }
             return result;
         }
+   
     }
 }
